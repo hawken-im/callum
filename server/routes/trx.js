5be793770fedc84e227fba6605c3e995625aef1a
hawken <askhaokun@gmail.com>
2023-01-29 14:09:05 +0800

    back to rum-demo

---
+++ server/routes/trx.js
@@ -0,0 +1,42 @@
+const router = require('koa-router')();
+const { assert, Errors, throws } = require('../utils/validator');
+const SDK = require('rum-sdk-nodejs');
+
+//router.post('/', sendTrx);
+router.post('/', receiveContent);
+
+router.get('/:trxId', get);
+
+async function receiveContent(ctx) {
+  const payload = ctx.request.body;
+  assert(payload, Errors.ERR_IS_REQUIRED('payload'));
+  console.log(payload);
+  ctx.response.body = 'success';
+}
+
+// async function sendTrx(ctx) {
+//   const payload = ctx.request.body;
+//   assert(payload, Errors.ERR_IS_REQUIRED('payload'));
+//   const group = SDK.cache.Group.list()[0];
+//   assert(group, Errors.ERR_IS_REQUIRED('group'));
+//   try {
+//     const res = await SDK.chain.Trx.send(group.groupId, payload);
+//     ctx.body = res;
+//   } catch (err) {
+//     console.log(err);
+//     throws('ERR_IS_REQUEST_FAILED');
+//   }
+// }
+
+async function get(ctx) {
+  const group = SDK.cache.Group.list()[0];
+  assert(group, Errors.ERR_IS_REQUIRED('group'));
+  try {
+    ctx.body = await SDK.chain.Trx.get(group.groupId, ctx.params.trxId);
+  } catch (err) {
+    console.log(err);
+    throws('ERR_IS_REQUEST_FAILED');
+  }
+}
+
+module.exports = router;
--- server/src/model/Model.ts
+++
@@ -1,3 +0,0 @@
-export * from './Question.js';
-export * from './Reply.js';
-export * from './User.js';
