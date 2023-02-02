d6b12ff04f9cdf4a33c3b53c9058a60482ee4280
hawken <askhaokun@gmail.com>
2023-02-02 12:48:27 +0800

    render projects on page

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -3,43 +3,30 @@ import { socketIo } from '../utils/socket';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { IProject, TrxStorage } from '../apis/types'
 
-// interface Props{
-//   key:string;
-//   project: IProject;
-// }
-
-// function ListItem(props: Props) {
-//   return <div>{props.project.content}</div>;
-// }
-
 function ProjectList() {
-  const [ids, setIds] = useState([] as string[]);
-  const [projectMap, setProjectMap] = useState({} as Record<string,IProject>)
+
+  const [projectMap, setProjectMap] = useState([] as IProject[])
   useEffect(() => {
     const onNewPost = (project: IProject) => {
       console.log(`Got a project: ${JSON.stringify(project)}`);
-      setIds((ids:string[])=> {ids.unshift(project.id);return ids});
-      setProjectMap(()=> {projectMap[ids[0]]=project;return projectMap})
-      console.log(`project map: ${JSON.stringify(projectMap)}, keys: ${Object.keys(projectMap)}`)
+      setProjectMap([project, ...projectMap]);
     }
     socketIo().on('project', onNewPost);
-  }, [ids,projectMap]);
+    return ()=> {
+      socketIo().off('project');
+    }
+  }, [projectMap]);
 
   return (
-    <>
-    <div>Come on</div>
     <div>
-      {Object.keys(projectMap).length === 0 ? `empty! ${Object.keys(projectMap)}` :
-        ids.map((id) => 
-        <div>
-           {projectMap[id].content}
+      {projectMap.length === 0 ? `empty!` :
+        projectMap.map((item) => 
+        <div key={item.id}>
+           {item.content}
         </div>
       )}
     </div>
-    </>
   );
 }
-
-
 
 export default ProjectList
